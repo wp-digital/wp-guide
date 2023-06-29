4d724880e09b439d92e893d21337ac93d2e42d75
ZhiyanOlexandr <zhiyan.olexandr@gmail.com>
2023-06-29 14:07:11 +0300

    rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '26002b38d93d9ad271bf');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'd2a927bd78ed08a457e9');
